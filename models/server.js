750005f9b20a105d06856247638d0c7ec820118f
Javier Echavez Chardaux <javier.echavez@ludycom.com>
2022-01-13 10:25:54 -0500

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const cors = require('cors');
+const { socketController } = require('../sockets/controller');
+
+class Server {
+
+    constructor() {
+        this.app    = express();
+        this.port   = process.env.PORT;
+        this.server = require('http').createServer(this.app);
+        this.io     = require('socket.io')( this.server );
+
+        this.paths  = { };
+
+        // Middlewares
+        this.middlewares();
+
+        // App routes
+        this.routes();
+
+        // Sockets
+        this.sockets();
+    }
+
+    middlewares() {
+        // CORS
+        this.app.use(cors());
+
+        // Public directory
+        this.app.use(express.static('public'));
+    }
+
+    routes() {
+
+        // this.app.use(this.paths.auth, require('../routes/auth'));
+
+    }
+
+    sockets() {
+        
+        this.io.on('connection', socketController) 
+    }
+
+    listen() {
+        this.server.listen(this.port, () => {
+            console.log(`Rest server listening at http://localhost:${this.port}`)
+        })
+    }
+}
+
+module.exports = Server;
